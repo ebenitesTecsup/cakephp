81680985f3d696dacd6659c9e84be1129046747d
ebenitesTecsup <ebenites@tecsup.edu.pe>
2016-02-27 14:00:08 -0500

    Cambios en el Home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,14 +7,14 @@ class HomeController extends AppController{
 
     public function index() {
         
-        $email = new Email('default'); // Seleccionar el profile a usar
-        $email->from(['tienda@tecsup.edu.pe' => 'Tienda Online'])
-            ->to('ebenites@tecsup.edu.pe')
-            ->subject('Correo desde CakePHP 3')
-            ->send('Contenido del correo ...');
-        
-        echo 'Correo enviado';
-        $this->autoRender = false; // Solo para este ejemplo no se usará una vista
+//        $email = new Email('default'); // Seleccionar el profile a usar
+//        $email->from(['tienda@tecsup.edu.pe' => 'Tienda Online'])
+//            ->to('ebenites@tecsup.edu.pe')
+//            ->subject('Correo desde CakePHP 3')
+//            ->send('Contenido del correo ...');
+//        
+//        echo 'Correo enviado';
+//        $this->autoRender = false; // Solo para este ejemplo no se usará una vista
 
         
     }
